27c64301aa58b4971fcc9971167937342d8a28c8
Niklas Sauter <niklas@n1ks.net>
2023-01-16 00:39:40 +0100

    Add nix-community substituter

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -1,12 +1,18 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-  ];
+  imports = [ ./hardware-configuration.nix ];
 
   nix = {
-    package = pkgs.nixFlakes;
+    settings = {
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
     extraOptions = "experimental-features = nix-command flakes";
   };
 
