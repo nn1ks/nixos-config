844e05f65455350d378aa0b7cd6b2c05b19f7699
Niklas Sauter <niklas@n1ks.net>
2023-01-16 03:14:14 +0100

    Set keyboard layout of console to "de"

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -47,6 +47,8 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
+  console.keyMap = "de";
+
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   # Enable the GNOME Desktop Environment.
