61ddadaedf89b2ca5098fb0e06f6e059bcc092a4
Niklas Sauter <niklas@n1ks.net>
2023-07-23 03:15:57 +0200

    Remove unnecessary permittedInsecurePackages

--- devices/mika/configuration.nix
+++ devices/mika/configuration.nix
@@ -24,8 +24,6 @@ in {
     ../../modules/services/mautrix-whatsapp.nix
   ];
 
-  nixpkgs.config.permittedInsecurePackages = [ "nodejs-14.21.3" "openssl-1.1.1u" ];
-
   boot.loader.grub = {
     enable = true;
     devices = [ "/dev/sda" ];
